c993220d0829d39be31e30156d49045ad362ffae
Rafael <rafaelmerlotto@gmail.com>
2025-12-11 14:25:05 +0100

    Some fixes

--- lib/resourcespace/client.rb
+++ lib/resourcespace/client.rb
@@ -137,7 +137,7 @@ module ResourceSpace
       }
 
       request_params[:authmode] = config.auth_mode if config.auth_mode
-      request_params.merge(params.transform_keys(&:to_s))
+      request_params.merge!(params.transform_keys(&:to_s))
 
       # Build query string for signing
       signing_params = request_params.reject { |_k, v| v.is_a?(Faraday::UploadIO) }
@@ -156,7 +156,7 @@ module ResourceSpace
 
       # Make the request
       response = if method == :get
-                   ordered_query = URI.encode_www_form(request_params.sort.to_h)
+                   ordered_query = URI.encode_www_form(signing_params.sort_by { |k, _| k.to_s })
 
                    connection.get("?#{ordered_query}")
                  elsif multipart
